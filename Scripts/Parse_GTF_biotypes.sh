3950b3b5cd42de04d8787a3a45c4ed7b56c3ceeb
Virginie Ricci <ninet93@hotmail.com>
2022-09-12 17:50:46 +0200

    Update Parse_GTF_biotypes.sh

--- Scripts/Parse_GTF_biotypes.sh
+++ Scripts/Parse_GTF_biotypes.sh
@@ -19,7 +19,7 @@ module load Python/3.5.2-goolf-1.7.20
 path_Onil='/NCBI_Orenil_GCF_001858045_2/ncbi-genomes-2020-01-13/'
 gtf_f='GCF_001858045.2_O_niloticus_UMD_NMBU_genomic.gtf'
 
-python Parse_GTF.py $gtf_f $path_Onil
+python Parse_GTF_biotypes.py $gtf_f $path_Onil
 
 
 #################################################################################################################################
